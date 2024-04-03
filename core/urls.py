acb82ecbcf8819530424cc498c7e1a1e4d8002ff
Mohammed Elgaily <mbajily@gmail.com>
2024-04-03 19:11:20 +0300

    Generate Mail APIs.

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("emails/<str:mailbox>", views.getMailbox),
+    path("emails/email/create", views.createEmail),
+    path("emails/email/<str:pk>/", views.getEmail),
+    path("emails/email/<str:pk>/update", views.updateEmail),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CompanySerializer, EmployeeSerializer, EmailSerializer, EmailFilesSerializer, DriveSerializer
-from .models import Company, Employee, Email, Email_File, Drive_File
+from .models import User, Company, Employee, Email, Email_File, Drive_File
+from django.db.models import Q
 
 
 @api_view(['GET'])
@@ -28,8 +29,108 @@ def getMailbox(request, mailbox):
         ).filter( Q(sender=request.user) |Q(recipients=request.user))
     
     else:
-        return JsonResponse({"error": "Invalid mailbox."}, status=400)
+        return Response({"error": "Invalid mailbox."}, status=400)
 
     # Return emails in reverse chronologial order
     serializer = EmailSerializer(emails, many=True)
     return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def getEmail(request, pk):
+    try:
+        email = Email.objects.get(id=pk)
+        serializer = EmailSerializer(email, many=False)
+        return Response(serializer.data)
+
+    except:
+        return Response({"error": "Email does not exist."}, status=404)
+
+
+
+@api_view(['PUT', 'DELETE'])
+def updateEmail(request, pk):
+
+    # Query for requested email
+    try:
+        email = Email.objects.get(user=request.user, id=pk)
+    except Email.DoesNotExist:
+        return Response({"error": "Email does not exist."}, status=404)
+
+    # Update whether email is read or should be archived
+    if request.method == "PUT":
+        # data = json.loads(request.body)
+        data = request.data
+        if data.get("read") is not None:
+            email.read = data["read"]
+        if data.get("archived") is not None:
+            email.archived = data["archived"]
+        if data.get("starred") is not None:
+            email.starred = data["starred"]
+        if data.get("deleted") is not None:
+            email.deleted = data["deleted"]
+        email.save()
+        serializer = EmailSerializer(email, many=False)
+        return Response(serializer.data, status=204)
+
+    elif request.method == "DELETE":
+        email.delete()
+        return Response({"message": "Email is deleted."}, status=204)
+    # email must be via GET or PUT
+    else:
+        return Response({
+            "error": "PUT or DELETE request required."
+        }, status=400)
+
+
+
+@api_view(['POST'])
+def createEmail(request):
+
+    # Composing a new email must be via POST
+    if request.method != "POST":
+        return Response({"error": "POST request required."}, status=400)
+
+    # Check recipient emails
+    # data = json.loads(request.body)
+    data = request.data
+    emails = [email.strip() for email in data.get("recipients").split(",")]
+    if emails == [""]:
+        return Response({
+            "error": "At least one recipient required."
+        }, status=400)
+
+    # Convert email addresses to users
+    recipients = []
+    for email in emails:
+        try:
+            user = User.objects.get(username=email)
+            recipients.append(user)
+        except User.DoesNotExist:
+            return Response({
+                "error": f"User with email {email} does not exist."
+            }, status=400)
+
+    # Get contents of email
+    subject = data.get("subject", "")
+    body = data.get("body", "")
+
+    # Create one email for each recipient, plus sender
+    users = set()
+    users.add(request.user)
+    users.update(recipients)
+    for user in users:
+        email = Email(
+            user=user,
+            sender=request.user,
+            subject=subject,
+            body=body,
+            read=user == request.user
+        )
+        email.save()
+        for recipient in recipients:
+            email.recipients.add(recipient)
+        email.save()
+
+    return Response({"message": "Email sent successfully."}, status=201)
