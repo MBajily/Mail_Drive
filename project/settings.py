b0d7e79ca93c316999cc9b58716c7d14bbb91894
Mohammed Elgaily <mbajily@gmail.com>
2024-04-04 23:24:02 +0300

    Add DRF Auth Token.

--- project/settings.py
+++ project/settings.py
@@ -45,10 +45,15 @@ INSTALLED_APPS = [
     'company',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,8 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +31,9 @@ urlpatterns = [
 
     # DRF APIs routes
     path('api/v1/', include('core.urls')),
+
+    # DRF Auth Token
+    path('api/v1/token/request/', obtain_auth_token),
 
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
